531718c34b9c7ec01219bf7766ebcbac95b5e753
Nick Hoo <442869088@qq.com>
2020-12-19 03:11:34 +0000

    Add files via upload

---
+++ C for Everyone Programming Fundamentals/week1/poem.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(void)
+{   
+    printf("But thy eternal summer shall not fade\n");
+    printf("Nor lose possession of that fair thou ow’st;\n");
+    printf("Nor shall Death brag thou wander’st in his shade,\n");
+    printf("When in eternal lines to time thou grow’st;\n");
+    printf("So long as men can breathe or eyes can see,\n");
+    printf("So long lives this, and this gives life to thee.\n");
+    
+    return 0;
+}  
